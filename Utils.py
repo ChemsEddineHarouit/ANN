added Genetic Algorithms in GA, and fixed MLP calculations in backprop

Utils.py
@@ -1,8 +1,33 @@
 import numpy as np 
 
 
-def sigmoid(x):
-    return 1/(1-np.exp(-x))
-
 def tanh(x):
     return np.tanh(x)
+
+def sigmoid_deriv(x):
+    return x*(1-x)
+
+def sigmoid(x):
+    y = 1/(1- np.exp(-x))
+    # print(x, y)
+    return y
+            
+def add_1_r(matrix):
+    l = np.shape(matrix)[1]
+    ones = np.ones((1,l))
+    new =  np.r_[ones, matrix]
+    return new
+
+def add_1_c(matrix):
+    l = np.shape(matrix)[0]
+    ones = np.ones((l,1))
+    new =  np.c_[ones, matrix]
+    return new
+
+def rem_1st_c(matrix):
+    new = matrix[:, 1:]
+    return new
+
+def rem_1st_r(matrix):
+    new = matrix[1:, :]
+    return new

main.py
@@ -1,28 +1,47 @@
-import pandas as pd
 import numpy as np
 from sklearn import datasets
-from MLP_3 import MLP_3
-
-np.random.seed(2)
+from MLP import MLP as MLP
 
 #Read Data
 iris = datasets.load_iris()
+
 X = iris.data
 D = iris.target
 
 
-
-# dataFrame = pd.read_csv('iris.data')
-
 #Train and Test set
-mask = np.random.rand(len(X)) < 0.8
-train = X[mask], D[mask]
-test = X[~mask], D[~mask]
+np.random.seed(1)
+mask = np.random.rand(len(X)) < 0.9
+trainData = trainX, trainD = X[mask], D[mask]
+testData = testX , testD  = X[~mask], D[~mask]
 
 #Create the Neural Network and train it
-mlp_3 = MLP_3(train)
-mlp_3.plotData()
-mlp_3.train()
+mlp = MLP(trainX, trainD, [2, 5], 0.1)
+mlp.train()
 
 #Predict with the test Data
-mlp_3.predict(test)
+Acc = mlp.predict(trainX, trainD)
+print("Accuracy = %d"%(Acc))
+mlp.plotErrors()
+print("Last error = %d"%(mlp.costs[-1]))
+
+
+# neurons = []
+# rates = [.001, .005, .01, .04, .08, .1, .4, .8, 1.2]
+# Acc = acc = 0
+# for i in range(1, 20):
+#     for j in range(1, 20):
+#         for k in range(1, 2):
+#             for r in rates:
+#                 mlp = MLP(trainX, trainD, [i, j], r)
+#                 mlp.train()
+#                 acc = mlp.predict(testX , testD)
+#                 if(acc == Acc):
+#                     neurons.append((i,j,k,r))
+#                 elif (acc > Acc):
+#                     Acc = acc
+#                     neurons = []
+#                     neurons.append((i,j,k,r))
+#                 print(  "\n\n\n\n", i, j, k, r)
+#                 print("Best accuracy is %d/100 with neurons:\nL1, L2, L3, rate\n%s"%(Acc, neurons[-1]))
+# print("Best accuracy is %d/100 with neurons:\nL1, L2, L3, rate\n%s"%(Acc, neurons))